f505ea4f05c45c422b6f02285ecbf6d191c1be15
Enrique Orozco <150449111+orozco21@users.noreply.github.com>
2024-11-23 10:12:46 -0500

    Update conexion.py

--- core/conexion.py
+++ core/conexion.py
@@ -1,12 +1,11 @@
 import mysql.connector
 
-config = {
+conexion = mysql.connector.connect(
     host="bq5khnloapqqfal7sr8u-mysql.services.clever-cloud.com",
     user="ug5aqugn1mlfkljc",
     password="fVbvJJnAybtXVb7ekEp6",
     database="bq5khnloapqqfal7sr8u"
-    'raise_on_warnings': True,}
+)
 
-cnx = mysql.connector.connect(**config)    
-cursor = cnx.cursor()
+
 
